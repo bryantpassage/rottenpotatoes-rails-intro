e8fa0d0457e37465fee1244540029c494f8ebf3b
bryantpassage <bryant.passage@gmail.com>
2021-02-08 20:34:21 +0000

    added ratings filter code in controller and model

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,9 +7,15 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.all
+    # @movies = Movie.all
     @all_ratings = Movie.all_ratings
-    @ratings_to_show = []
+    if !params.include?(:ratings) then
+      @ratings_to_show = []
+    else
+      @ratings_to_show = params[:ratings].keys
+    end
+    # @movies = Movie.with_ratings(params[:ratings].keys)
+    @movies = Movie.with_ratings(@ratings_to_show)
   end
 
   def new
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -3,4 +3,17 @@ class Movie < ActiveRecord::Base
     def self.all_ratings
         @@all_ratings =  ['G', 'PG', 'PG-13', 'R']
     end
+    
+    # show movies with certain ratings
+    def self.with_ratings(ratings_list)
+        # if ratings_list is an array such as ['G', 'PG', 'R'], retrieve all
+        #  movies with those ratings
+        # if ratings_list is nil, retrieve ALL movies
+        if ratings_list.length() == 0 then
+            return self.all
+        else
+            # self.find_all_by_rating
+            return self.where(rating: ratings_list)
+        end
+    end
 end
